44ee9cd5af0e69ee384bd2108f92047450c83eed
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-02-28 22:13:31 +0100

    feat: Prepare for deployment, add gunicorn and Procfile

--- basilandthyme/settings.py
+++ basilandthyme/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sxwy$fogm5j9$8r#^989sv*v=2!tm49o!t4gp9dxqb$l+*^8^1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
