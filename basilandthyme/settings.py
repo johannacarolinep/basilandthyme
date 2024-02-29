66e4ced64d3d5f1fb0f000b025bb6b86a989e225
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-02-29 10:48:02 +0100

    feat: Add secure secret key

--- basilandthyme/settings.py
+++ basilandthyme/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sxwy$fogm5j9$8r#^989sv*v=2!tm49o!t4gp9dxqb$l+*^8^1'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
