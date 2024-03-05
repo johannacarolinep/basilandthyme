2ecd92f569ca5d55140110abbf398541d3ee6244
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-03-05 17:26:59 +0100

    feat: Add eventlisteners to buttons below search bar

---
+++ static/js/script.js
@@ -0,0 +1,38 @@
+/* Wait for page to load before initializing script */
+document.addEventListener("DOMContentLoaded", initializeScript);
+
+function initializeScript() {
+    const categoryButtons = document.getElementsByClassName('btnCategory');
+    const btnAllRecipes = document.getElementById('btnAllRecipes');
+
+    for (let i = 0; i < categoryButtons.length; i++) {
+        categoryButtons[i].addEventListener('click', addCategoryQuery);
+    }
+
+    btnAllRecipes.addEventListener('click', addAllQuery);
+}
+
+function addCategoryQuery(event) {
+    const currentUrl = window.location.href;
+    let category = event.currentTarget.innerHTML.toLowerCase();
+    if (category === "see all") {
+        category = "all"
+    };
+    console.log(category);
+
+    // If URL does not contain /recipes, append "/recipes/?q=category"
+    if (currentUrl.indexOf('/recipes') === -1) {
+        window.location.href = currentUrl.split('?')[0].split('#')[0] + '/recipes/?q=' + category;
+    } else {
+        // If URL contains /recipes, remove any existing query string
+        const baseUrl = currentUrl.split('?')[0].split('#')[0];
+
+        // if there was a parameter
+        if (baseUrl.endsWith('/recipes/')) {
+            window.location.href = baseUrl + '?q=' + category;
+        } else {
+            // if there was no parameter
+            window.location.href = baseUrl + '/?q=' + category;
+        }
+    }
+}
