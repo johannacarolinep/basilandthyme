23a3f2e30fb9567d7524e6721fee4bc842470e76
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-03-13 11:43:51 +0100

    feat: Add more tests for the RecipeDetailView

--- recipe_book/test_views.py
+++ recipe_book/test_views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
-from . import views
 from .models import Recipe, Favourite
 
 
@@ -9,6 +8,9 @@ from .models import Recipe, Favourite
 class TestRecipeBookViews(TestCase):
 
     def setUp(self):
+        """
+        Set up test mock data including a mock user, recipe, and favorite.
+        """
         # Create a mock user
         self.user = User.objects.create_user(
             username="testuser", email="test@test.com", password="testpassword"
@@ -29,6 +31,53 @@ class TestRecipeBookViews(TestCase):
             user=self.user, recipe=self.recipe)
 
     def test_render_recipe_detail_page_with_invalid_slug(self):
+        """
+        Test rendering a recipe detail page with an invalid slug.
+        """
         response = self.client.get(reverse(
             'recipe_detail', kwargs={'slug': 'non-existing-slug'}))
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(
+            response.status_code, 404, msg="Status code is not 404")
+
+    def test_render_recipe_detail_page_when_not_logged_in(self):
+        """
+        Test rendering a recipe detail page when the user is not logged in.
+        """
+        self.client.logout()  # log out mock user
+        response = self.client.get(reverse(
+            'recipe_detail', kwargs={'slug': 'test-recipe'}))
+        self.assertEqual(
+            response.status_code, 200, msg="Status code is not 200")
+        # Assert that 'is_favourite' is False since user is not logged in
+        self.assertFalse(
+            response.context['is_favourite'], msg="Recipe is favourite")
+
+    def test_render_recipe_detail_page_with_favourite(self):
+        """
+        Test rendering a recipe detail page when the recipe is favourited by
+        the logged in user.
+        """
+        response = self.client.get(reverse(
+            'recipe_detail', kwargs={'slug': 'test-recipe'}))
+        self.assertEqual(
+            response.status_code, 200, msg="Status code is not 200")
+        self.assertTemplateUsed(response, 'recipe_book/recipe-page.html')
+        self.assertEqual(
+            response.context['recipe'].title, 'Test Recipe', msg="Wrong title")
+        self.assertEqual(
+            response.context['is_favourite'], True, msg="Should be favourite")
+
+    def test_render_recipe_detail_page_with_nonfavourited_recipe(self):
+        """
+        Test rendering a recipe detail page when the recipe is not favorited
+        by the logged in user.
+        """
+        # Remove the favourite for the user
+        self.favourite.delete()
+        response = self.client.get(reverse(
+            'recipe_detail', kwargs={'slug': 'test-recipe'}))
+        self.assertEqual(
+            response.status_code, 200, msg="Status code is not 200")
+        # Assert that 'is_favourite' is False since recipe is not favourited
+        self.assertFalse(
+            response.context['is_favourite'], msg="Incorrectly favourited")
