7da4160784c04b9d9c0999d8b4ab44a4e128e4d8
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-03-30 11:48:38 +0100

    feat: Adjust FeaturesListView to return the right objects

--- recipe_book/views.py
+++ recipe_book/views.py
@@ -95,6 +95,33 @@ class RecipeListView(ListView):
 class FeaturesListView(ListView):
     model = Recipe
     template_name = 'recipe_book/index.html'
+    context_object_name = 'context'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Annotate recipes with additional details
+        base_queryset = Recipe.objects.filter(status=1).annotate(
+            avg_rating=models.functions.Coalesce(models.Avg('rating_recipe__rating'), models.Value(0.0)),
+            ratings_count=models.Count('rating_recipe'),
+        )
+
+        # if logged in user, annotate recipes with users rating of recipe
+        user = self.request.user
+        if user.is_authenticated:
+            base_queryset = base_queryset.annotate(
+                user_rating=models.F('rating_recipe__rating')
+                )
+
+        # The 4 recipes with the highest rating
+        rating_queryset = base_queryset.order_by('-avg_rating')[:4]
+        date_queryset = base_queryset.order_by('-created_on')[:4]
+        context['recipes_by_rating'] = rating_queryset
+        context['recipes_by_date'] = date_queryset
+        context['user_favorites'] = Favourite.get_user_favourite_ids(user)
+        context['stars_range'] = range(1, 6)
+
+        return context
 
 
 class RecipeDetailView(DetailView):
