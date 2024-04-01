54eb9b01c57599dc394e4f4327564b69a2ad2db5
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2024-04-01 23:22:18 +0200

    feat: Adjust the Recipe admin page to show instructions

--- recipe_book/admin.py
+++ recipe_book/admin.py
@@ -11,9 +11,40 @@ class RecipeAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_on', 'category',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content', 'ingredients',)
+    fieldsets = [
+        (
+            "Recipe details",
+            {
+                "fields": ["title", "slug", "category", "status", "author"],
+                "classes": ["wide"],
+            },
+        ),
+        (
+            "Recipe image",
+            {
+                "fields": ["feature_image", "alt_text"],
+                "description": "Use image of dimension 3:2 (width:height) if" +
+                " possible. Images may otherwise be cropped. " + "<br>" +
+                "Please provide a custom alt text (short image description).",
+            },
+        ),
+        (
+            "Content and ingredients",
+            {
+                "fields": ["content", "ingredients", "teaser"],
+                "description": "Use only the provided formatter." +
+                "<strong>DO NOT</strong> copy paste pre-formatted content.",
+            },
+        )
+    ]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'author', 'approved', 'created_on')
+    list_filter = ('approved',)
 
 
 # Register your models here.
-admin.site.register(Comment)
 admin.site.register(Favourite)
 admin.site.register(Rating)
